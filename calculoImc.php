78395f0dfc1a32ecceedca162f8c3076507af482
bielCoder <gabrielhenrique9253@gmail.com>
2022-05-07 11:22:55 -0300

    Aprendi a realizar o calculo de massa corporal,foi adicionado um novo arquivo.

--- calculoImc.php
+++ calculoImc.php
@@ -1,1 +1,15 @@
-calculoImc
+<?php
+		// Tarefa de retorna o calculo IMC
+		function calculaIMC(){
+			$altura = 1.80;
+			$peso = 70;
+			$result = $peso / ($altura ^ 2);
+			
+			if($result < 18.5){
+				echo "Você está abaixo do peso!";
+			}else{
+				echo "Você não está abaixo do peso!";
+			}
+		}
+
+		return calculaIMC();
