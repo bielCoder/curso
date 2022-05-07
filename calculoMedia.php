00f88c36429b07f0bcf6f40a682e05ed3e949a40
bielCoder <gabrielhenrique9253@gmail.com>
2022-05-07 12:44:48 -0300

    Aprendi a calcular a média,foi adicionado um arquivo

---
+++ calculoMedia.php
@@ -0,0 +1,15 @@
+<?php
+
+		function calculaMedia(){
+			$primeira_nota = 5.8;
+			$segunda_nota = 7.3;
+
+			$media = ($primeira_nota + $segunda_nota) / 2;
+
+				
+			echo $media;
+
+
+		}
+
+		return calculaMedia();
